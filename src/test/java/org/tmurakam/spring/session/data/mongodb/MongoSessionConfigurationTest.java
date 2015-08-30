8ecf2d9aeef1a06e4f12b334ca55f8a011f31fbe
Takuya Murakami <tmurakam@tmurakam.org>
2015-08-30 14:39:42 +0900

    update mongodb driver

--- src/test/java/org/tmurakam/spring/session/data/mongodb/MongoSessionConfigurationTest.java
+++ src/test/java/org/tmurakam/spring/session/data/mongodb/MongoSessionConfigurationTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import static org.junit.Assert.assertSame;
 
 /**
- * Created by tmurakam on 15/08/29.
+ * MongoSession Configuration Test
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class,
