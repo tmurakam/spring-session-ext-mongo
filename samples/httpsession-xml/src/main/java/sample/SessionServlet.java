2e24e4528f81cd77e2b1dc31bf28933fe0388287
Takuya Murakami <tmurakam@tmurakam.org>
2022-12-09 22:53:19 +0900

    fix(sample): Update sample
    - add maven war plugin
    - replace jakartaee-web-api artifact

--- samples/httpsession-xml/src/main/java/sample/SessionServlet.java
+++ samples/httpsession-xml/src/main/java/sample/SessionServlet.java
@@ -22,15 +22,15 @@ import java.io.IOException;
 // tag::class[]
 public class SessionServlet extends HttpServlet {
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String attributeName = req.getParameter("attributeName");
-		String attributeValue = req.getParameter("attributeValue");
-		req.getSession().setAttribute(attributeName, attributeValue);
-		resp.sendRedirect(req.getContextPath() + "/");
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String attributeName = req.getParameter("attributeName");
+        String attributeValue = req.getParameter("attributeValue");
+        req.getSession().setAttribute(attributeName, attributeValue);
+        resp.sendRedirect(req.getContextPath() + "/");
+    }
 
-	private static final long serialVersionUID = 2878267318695777395L;
+    private static final long serialVersionUID = 2878267318695777395L;
 }
 
 // end::class[]
